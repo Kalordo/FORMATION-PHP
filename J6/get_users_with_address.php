9ac47acadb2d3db6df1af615909873e468637444
Kalordo <yohann.leperson@gmail.com>
2025-06-11 15:35:45 +0200

    kalordo
    edit etape 5

--- J6/get_users_with_address.php
+++ J6/get_users_with_address.php
@@ -2,7 +2,7 @@
 
 require 'connexion.php';
 
-$query = $db->prepare('SELECT * FROM users JOIN address ON users.id = address.id');
+$query = $db->prepare('SELECT * FROM users JOIN address ON users.address = address.id');
 $query->execute();
 
 $usersAddress = $query->fetchAll(PDO::FETCH_ASSOC);
