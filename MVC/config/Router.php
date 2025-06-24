940c0f2aa3492f3d92b9c01784bec0fcb1f89dec
Kalordo <yohann.leperson@gmail.com>
2025-06-24 10:18:20 +0200

    Ajout de la classe PageController et des méthodes pour gérer les routes, ainsi que des fichiers de template pour les pages d'accueil, à propos, de contact et d'erreur 404.

--- MVC/config/Router.php
+++ MVC/config/Router.php
@@ -6,11 +6,14 @@ class Router {
 
     public function handleRequest(array $get) : void {
         if (isset($get["route"]) && ($get["route"]) === "a-propos" ) {
-
+            $controller = new PageController();
+            $controller->about();
         } else if (!isset($get["route"])) {
-
+            $controller = new PageController();
+            $controller->home();
         } else {
-            
+            $controller = new PageController();
+            $controller->notFound();
         }
     }
 }
